b8c4b84448e2870bb596662520db0977afcf68d7
snizhokma <snizhokma@gmail.com>
2021-12-27 23:48:13 +0200

    Added scene transition in the game over menu

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     public void ReturnToMainMenu()
     {
-        //ToDo
+        SceneManager.LoadScene(2);
     }
 }
