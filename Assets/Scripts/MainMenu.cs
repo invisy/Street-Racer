15c57f49e1b0fca65eb2c294fe36624c6c15f422
snizhokma <snizhokma@gmail.com>
2021-12-27 23:44:01 +0200

    Added scene transition

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void Play(int sceneNumber)
     {
-        //ToDo
+        SceneManager.LoadScene(sceneNumber);
     }
 
     public void Exit()
